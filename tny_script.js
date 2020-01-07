45fd706852cbcfdbbdd0974941c8d23df287d438
Colby <cabbott195@west-mec.org>
2020-01-06 18:22:46 -0700

    Changes to JS

---
+++ tny_script.js
@@ -0,0 +1,20 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Colby Abbott
+   Date:   1/20/19
+
+*/
+
+console.log("Hello World!");
+
+console.log("4");
+
+console.log("2+1");
+
+console.log(3+"pies");
+
+console.log("4/0");
